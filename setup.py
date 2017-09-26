b7753e98dad72c06c2e6df70ca597dae7f600d0f
szhao <sheng.zhao@calix.com>
2017-09-26 16:30:24 +0800

    Update setup personal message

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name="pygennf",
-      version="0.1",
+      version="0.11",
       description="UDP packets producer with scapy",
-      author="Ana Rey",
-      author_email="anarey@redborder.com",
-      url="https://github.com/redBorder/",
+      author="Sheng Zhao",
+      author_email="sheng.zhao@calix.com",
+      url="https://github.com/9nehS/",
       license="AGPL",
       scripts=["src/pygennf_v5.py", "src/pygennf_v9.py", "src/pygennf_v10.py" ],
       packages=['rb_netflow'],
