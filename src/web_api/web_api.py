8aa88228c27e22428f4abbfcbc4b77a9ee29d4d0
szhao <frank_zs@aliyun.com>
2017-12-12 17:00:28 +0800

    Add web_api.py

---
+++ src/web_api/web_api.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from flask import Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def help():
+    return jsonify(
+        # 'API (application/json)': 'PATH, notes ?org-id=<orgId> currently required',
+        {'Cache clean': '/marketing/cache/clean?org-ids=<org-ids>',
+         'Cache clean and rebuild': '/marketing/cache/clean-rebuild?org-ids=<org-ids>&rebuild-table=true',
+         'Cache status': '/marketing/cache/status',
+         'On board': '/marketing/onboard?org-ids=<org-ids>',
+         'On board status': '/marketing/onboard/status',
+         'Delayed scheduled rebuild task': 'add:/marketing/cache/rebuild/operation?opt=add&org-ids=<org-ids> '
+                                           'delete:/marketing/cache/rebuild/operation?opt=delete&org-ids=<org-ids>',
+         'Delayed task status': '/marketing/cache/rebuild/status?org-id=<org-ids>'
+         })
+
+def start():
+    app.run(host='0.0.0.0')
