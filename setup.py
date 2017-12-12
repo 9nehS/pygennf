baa615ed38462aea22d16786fc6036dd9c164cd2
szhao <frank_zs@aliyun.com>
2017-12-12 17:08:03 +0800

    Add web_api.py in setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(name="pygennf",
       author_email="sheng.zhao@calix.com",
       url="https://github.com/9nehS/",
       license="AGPL",
-      scripts=["src/pygennf_v9.py", "src/pygennf_v9_multi_threads.py"],
+      scripts=["src/pygennf_v9.py", "src/pygennf_v9_multi_threads.py", "src/web_api/web_api.py"],
       packages=['rb_netflow'],
       install_requires=[
           'scapy',
       ]
-) 
+      )
