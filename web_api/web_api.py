6bcb93d997ae782f222ae19cbbd2b051317e7fbc
szhao <frank_zs@aliyun.com>
2017-12-13 16:30:52 +0800

    Add method send()

--- web_api/web_api.py
+++ web_api/web_api.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+import threading
+
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
 
@@ -15,6 +17,37 @@ def help():
                                            'delete:/marketing/cache/rebuild/operation?opt=delete&org-ids=<org-ids>',
          'Delayed task status': '/marketing/cache/rebuild/status?org-id=<org-ids>'
          })
+
+
+@app.route('/pygennf/send', methods=['POST'])
+def send():
+    if not request.json:
+        abort(404)
+    ip_src = request.json['ip_src']
+    ip_dst = request.json['ip_dst']
+    port_src = int(request.json['port_src'])
+    port_dst = int(request.json['port_dst'])
+    flow_data_list = get_flow_data_list(request.json['flows-data'], DEFAULT_FLOW_DATA)
+    pkt_count = int(request.json['pkt_count'])
+    time_interval = request.json['time_interval']
+    print 'Thread %s is running...' % threading.current_thread().name
+    t = threading.Thread(target=start_send, name='SendingThread', args=(ip_src, ip_dst, port_src, port_dst,
+                                                                        flow_data_list, pkt_count, time_interval))
+    # t.do_run = True
+    # t.setDaemon(True)
+    t.start()
+    while True:
+        t.join(5)
+        if not t.isAlive():
+            break
+
+    print 'Thread %s ended.' % threading.current_thread().name
+
+
+@app.route('/pygennf/status')
+def status():
+    pass
+
 
 def start():
     app.run(host='0.0.0.0', port=9080)
