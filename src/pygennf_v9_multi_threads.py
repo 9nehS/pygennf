8fac6367b85f76eb18b4008dd59e6a6932f35bba
szhao <frank_zs@aliyun.com>
2017-12-14 15:09:31 +0800

    Updated logger output in get_flow_data_list()

--- src/pygennf_v9_multi_threads.py
+++ src/pygennf_v9_multi_threads.py
@@ -253,17 +253,21 @@ def main():
 
 
 def get_flow_data_list(args_flows_data, default_flow_data):
-    print 'get_flow_data_list() start...'
-    print 'type(args_flows_data):', type(args_flows_data)
-    print 'args_flows_data: %s' % args_flows_data
+    # print 'get_flow_data_list() start...'
+    logger.debug('Enter get_flow_data_list()...')
+    # print 'type(args_flows_data):', type(args_flows_data)
+    # print 'args_flows_data: %s' % args_flows_data
     flow_data_list = args_flows_data.split(',')
     flow_data_list = map(str.strip, flow_data_list)
     flow_data_list = filter(valid_flow_data, flow_data_list)
     if len(flow_data_list) == 0:
-        print 'No valid flow data list, default flow data list will be used...'
-        print "Default flow data: %s" % (default_flow_data)
+        # print 'No valid flow data list, default flow data list will be used...'
+        logger.warn('No valid flow data list, default flow data list will be used...')
+        # print "Default flow data: %s" % (default_flow_data)
+        logger.info('Default flow data: %s' % (default_flow_data))
         flow_data_list.append(default_flow_data)
-    print 'flow_data_list before return:', flow_data_list
+    # print 'flow_data_list before return:', flow_data_list
+    logger.debug('flow_data_list before return: %s' % flow_data_list)
     return flow_data_list
 
 
